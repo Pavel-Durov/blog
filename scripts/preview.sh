112834b673ca41bd9dafe82bec3f5c10a6c46b3d
Pavel Durov <pav3ldurov@gmail.com>
2022-11-18 18:20:40 +0000

    chore: previews

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -12,7 +12,8 @@ ascii-themes generate --font='Cyberlarge' --themeName='Dark+' --text="Delivery"
 echo ""
 echo ""
 echo "$ subtitle"
-echo "Application deployment strategies: configurable and gradual deployments with minimised downtime."
+echo ""
+echo "  Application deployment strategies: configurable and gradual deployments with minimised downtime."
 
 for _ in {1..10}
 do
